1e2854cfc253372c9fc55e0ccbdd27dd18635ba2
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-01-05 20:45:39 +0100

    added custom colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 module.exports = {
   content: ['./public/*.html'],
+  darkMode: 'class',
   theme: {
     extend: {
-     
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
+      },
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'],
+        opensans: ['Open sans', 'sans-serif'],
+      },
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('../public/images/logo-dark-mode.svg')",
+        'logo-light-mode': "url('../public/images/logo-light-mode.svg')",
+        'curvy-dark-mode': "url('../public/images/curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('../public/images/curvy-light-mode.svg')",
+      }),
     },
+    variants: {
+      extend: {
+        backgroundImage: ['dark']
+      },
+    }
   },
   plugins: [],
 }
